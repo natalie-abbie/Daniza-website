9b69468a42ecb0fe7f519d7eb2d308d2da24c5ae
DANIZALTD <natalie-abbie>
2021-05-25 13:55:48 +0300

    add files

--- js/main.js
+++ js/main.js
@@ -304,3 +304,38 @@
 
 }());
 
+
+
+
+// counter
+
+$.fn.jQuerySimpleCounter = function( options ) {
+	var settings = $.extend({
+		start:  0,
+		end:    100,
+		easing: 'swing',
+		duration: 400,
+		complete: ''
+	}, options );
+
+	var thisElement = $(this);
+
+	$({count: settings.start}).animate({count: settings.end}, {
+		duration: settings.duration,
+		easing: settings.easing,
+		step: function() {
+			var mathCount = Math.ceil(this.count);
+			thisElement.text(mathCount);
+		},
+		complete: settings.complete
+	});
+};
+
+
+$('#number1').jQuerySimpleCounter({end: 10,duration: 3000});
+$('#number2').jQuerySimpleCounter({end: 70,duration: 3000});
+$('#number3').jQuerySimpleCounter({end: 15,duration: 2000});
+$('#number4').jQuerySimpleCounter({end: 30,duration: 2500});
+
+
+
